f0e5fc8b17f330354691be345e6d5680ab853556
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-03 12:15:58 +0000

    Dodano zadania 1-4 z KartaPracy2.py

---
+++ KartaPracy2.py
@@ -0,0 +1,29 @@
+# Zad.1
+
+a = int(input("Podaj a:"))
+if a%3==0:
+  print("TAK")
+else:
+  print("NIE")
+
+#Zad.2
+
+a = int(input("Podaj a:"))
+if a>=100 and a<1000 and a%17==0:
+    print("TAK")
+else:
+  print("NIE")
+#Zad.3
+a = int(input("podaj wiek:"))
+if a>=18:
+  print("TAK")
+  else:
+  print("NIE")
+
+#Zad.4
+
+limit = int(input("Podaj masę pojazdu:"))
+if limit>=20:
+  print("Nie możesz wjechać")
+else:
+print("Możesz wjechać")
