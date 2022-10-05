02d458f101f8755c1a3975111326925b3006e836
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-05 06:04:07 +0000

    Ukończono plik KartaPracy2.pdf

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -14,10 +14,10 @@ if a>=100 and a<1000 and a%17==0:
 else:
   print("NIE")
 #Zad.3
-a = int(input("podaj wiek:"))
-if a>=18:
+wiek = int(input("podaj wiek:"))
+if wiek>=18:
   print("TAK")
-  else:
+else:
   print("NIE")
 
 #Zad.4
@@ -26,4 +26,30 @@ limit = int(input("Podaj masę pojazdu:"))
 if limit>=20:
   print("Nie możesz wjechać")
 else:
-print("Możesz wjechać")
+  print("Tak możesz wjechać")
+
+#Zad.5
+
+a, b, c = map(int, input().split())
+if(a < c and b>c) or (b < c and a > c):
+ print("Tak")
+else:
+  print("Nie")
+
+#Zad.6
+
+a = int(input("Podaj dowolną liczbę"))
+p = int(input("Podaj liczbę pierwszą"))
+if ((a**2 - a) % p) == 0:
+  print("tak spełnia MTF")
+else:
+  print("Nie spełnia")
+
+
+#Zad.7
+
+p, k, s = map(int, input().split())
+if 3*s + p >= k:
+  print("Tak")
+else:
+  print("Nie")
