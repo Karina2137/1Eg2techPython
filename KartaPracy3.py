92266d1e8c229f50aeeb6df5e8dd798a249f32f0
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-12 06:29:46 +0000

    Dodanie zadań 2-6 do KartyPracy3.py

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,4 +1,4 @@
-# # Ankieta
+#Ankieta
 
 # x = int(input("Podaj liczbę"))
 # z = int(input("Podaj drugą liczbę"))
@@ -7,36 +7,61 @@
 # else:
 #   print("NIE")
 
-# # Zad.1
+#Zad.1
 # a = int(input())
 # for i in range(a):
 #   print(i**3 + 3, end=" ")
 
-# print(list(range(10)))
-# print(list(range(5)))
-# print(list(range(2,10,2)))
-# print(list(range(12,10)))
-# print(list(range(12,10,-1)))
-# print(list(range(111, 1, -2)))
+#Zad.2
+# for i in range(105,1000,15):
+#   print(i, end=" ")
 
+#Zad.3
+# n = int(input())
 # for i in range(1,n+1):
+#   if n % i == 0:
+#     print(i, end=" ")
 
-# for i in range(,10,2):
-#   print(i**2, end=" ")
+#Zad.4
+# suma = 0
+# for i in range(10,100):
+#   suma = suma + i
+# print(suma)
 
-# L = [0,1,2,3,4,5,6]
-# print(L[:6:2])
+#Zad.5
+# n = int(input())
+# suma = (n*(n+1)//2)
 
-#1 pętla
-for i in range(10,22): print(i, end=" ")
-#2 pętla
-for i in range(15,31, 2): print(i, end=" ")
-#3 pętla
-for i in range()
+# for i in range(n-1):
+#   x = int(input())
+#   suma = suma - x
+# print("Nie podałeś:",suma)
+
+# Zad.6
+a = 0
+b = 1
+n = int(input("Podaj ile razy:"))
+print(a,end=" ")
+print(b,end=" ")
+for i in range(n-2):
+  a, b = b, a+b
+  print(b, end=" ")
 
 
+# pętla liczb dwucyfrowych od 10 do 21
+# for i in range(10,22): print(i, end=" ")
 
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+# for i in range(15,32, 2): print(i, end=" ")
 
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# for i in range(99,10,-1): print(i, end=" " )
+
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# for i in range(10,100,1): print(109-i, end=" ")
+
+# pętla liczb 3-cyfrowych podzielnych przez 20
+# for i in range(100,999,20): print(i, end=" ")
 
 
 
