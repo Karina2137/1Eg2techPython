db28d9d45cb2c8e2c2b22fc5e96a627a0a65dc73
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-15 12:34:26 +0000

    Ukończono KartaPracy2a.py

---
+++ KartaPracy2a.py
@@ -0,0 +1,67 @@
+# Zad.1
+# a = int(input("Podaj pirewszą liczbę:"))
+# b = int(input("Podaj drugą liczbę:"))
+# if (a + b) / 2 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad.2
+# a = int(input("Podaj pierwszą liczbę:"))
+# g = int(input("Podaj drugą liczbę:"))
+# if (a + g) / 2 > (a * g) ** (1 / 2):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad.3
+# k = int(input("Podaj pierwszą liczbę:"))
+# l = int(input("Podaj drugą liczbę:"))
+# m = int(input("Podaj trzecią liczbę:"))
+# if k == l:
+#   print("TAK", k, l)
+# else:
+#   if k == m:
+#     print("TAK", k, m)
+# if l == m:
+#   print("TAK", l, m)
+# else:
+#   if l != m and k != l and k != m:
+#     print("NIE")
+
+# Zad.4
+# a = int(input("Podaj pirewszą liczbę:"))
+# b = int(input("Podaj drugą liczbę:"))
+# c = int(input("Podaj trzecią liczbę:"))
+# d = int(input("Podaj czwartą liczbę:"))
+# if (a < b and a < c and a < d ): print("Najmniejszą z nich jest",a)
+# else:
+#   if (b < a and b < c and b < d ): print("Najmniejszą z nich jest",b)
+# if (c < a and c < b and c < d ): print("Najmniejszą z nich jest",c)
+# else:
+#   if (d < a and d < c and d < c ): print("Najmniejszą z nich jest",d)
+
+#Zad. 5
+# a = int(input("Podaj pierwszy bok:"))
+# b = int(input("Podaj drugi bok:"))
+# c = int(input("Podaj trzeci bok:"))
+# if a + b > c and a + c > b and b + c > a:
+#   print("Podane liczby spełniają nierówność trójkąta.")
+# else:
+#   print("Podane liczby nie spełniają nierówność trójkąta.")
+
+# Zad.6
+# a = int(input("Podaj pierwszy kąt trójkąta:"))
+# b = int(input("Podaj drugi kąt trójkąta:"))
+# c = int(input("Podaj trzeci kąt trójkąta:"))
+# if a < 90 and b < 90 and c < 90 and (a + b + c) == 180: print("Trójkąt o podanych kątach jest ostrokątny.")
+# else:
+#   if a == 90 or b == 90 or c == 90 and (a + b + c) == 180: print("Trójkąt o podanych kątach jest prostrokątny.")
+# if a > 90 or b > 90 or c > 90 and (a + b + c) == 180: print("Trójkąt o podanych kątach jest rozwartokątny.")
+# else:
+#   if (a + b + c) != 180: print("Trójkąt o podanych kątach nie istnieje.")
+
+
+
+
+
