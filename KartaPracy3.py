8cd8be3c67945e6232735f44e63a3388ef3aa783
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-17 12:15:28 +0000

    Dodanie POST do KartaPracy3.py

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -65,4 +65,24 @@
 
 
 
-  
+# POST - utrwalenie pętli
+
+# Pętle for liczb trzycyforowych podzielnych przez 13 (2 sposoby)
+for i in range(104,1000,13):
+  print(i, end=" ")
+
+for i in range(100,1000):
+  if i % 13 == 0:
+    print(i, end=" ")
+# Pętle for liczb dwucyfrowych parzystych (3 sposoby)
+for i in range(10,99,2): 
+  print(i, end=" ")
+
+for i in range(10,100):
+ if i % 2 == 0:
+    print(i, end=" ")
+
+for i in range(5, 50):
+  print(i * 2, end=" ")
+# Pętle for potęg cyfr: WY: 0, 1, 4, 9, 16 .. 81 (2 sposoby printa)
+
