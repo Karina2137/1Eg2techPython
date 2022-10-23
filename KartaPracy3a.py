88ca4eb95fb4da89aea6ca009ce49c44a7a4aacb
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-23 11:56:31 +0000

    .

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -4,12 +4,21 @@
 #   print("*-|", end="")
 
 #Zad.2
-n = int(input())
-for i in range(1,n+1):
-  print("*"*i, end=" ")
-  if i%2:
-    print("||",end="")
-  else:
-    print("--",end="")
-
+# n = int(input())
+# for i in range(1,n+1):
+#   print("*"*i, end="")
+#   if i%2:
+#     print("||",end="")
+#   else:
+#     print("--",end="")
+# ----
 # Zad.3
+# n = int(input())
+# for i in range(1,n+1):
+#   print("*", end="")
+#   if i%2:
+#     print("|"*i, end="")
+#   else:
+#     print("-"*i, end="")
+
+# Zad.4
