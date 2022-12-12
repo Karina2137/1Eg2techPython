f8b06e0c75870e18f788cf3ae1e8da8d6698d956
23kp <114558445+Karina2137@users.noreply.github.com>
2022-12-12 13:15:50 +0000

    .

---
+++ KartyPracy/KartaPracy6a.py
@@ -0,0 +1,63 @@
+# print("Zadanie 1")
+# x = int(input())
+# wynik = 0 
+# while x > 0:
+#   wynik = wynik + x%10
+#   x = x // 10
+# print(wynik)
+
+# print()
+
+# print("Zadanie 2")
+# from math import sqrt
+# a = int(input())
+# for i in range(2,):
+
+
+
+
+  
+
+# print()
+
+# print("Zadanie 3")
+# n = int(input())
+# suma = 0
+# for i in range(1,n):
+#   if n % i == 0:
+#     suma += i
+# if suma == n:
+#   print("Liczba jest doskonała")
+# else:
+#   print("Liczba nie jest doskonała
+
+# print("Zadanie 5")
+h = int(input())
+for i in range(10,20):
+  x = h 
+  y = i
+  while y > 0 :
+    x, y =y, x % y
+  if x == 1:
+    print(n,i)
+
+# print("Zadanie 6")
+n = int(input())
+n = int(input())
+
+
+
+
+
+
+
+
+
+
+# n = int(input())
+# flaga = True
+# for i in range(2, n):
+#   if n % i==0:
+#     flaga=False
+# if flaga:
+#   print("l")
