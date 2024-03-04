10117efc3375170de0521b8d43ed8ae73ad3fc3b
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2024-03-04 08:42:19 +0100

    Update MonteCarlo.py

--- Algorytmy/MonteCarlo.py
+++ Algorytmy/MonteCarlo.py
@@ -14,3 +14,20 @@ for i in range(liczba_losowan):
 print("Punkty w/poza: ", punkty_w_kole, "/", liczba_losowan - punkty_w_kole)
 liczba_pi =  4 * punkty_w_kole / liczba_losowan
 print("Wartość pi: ", liczba_pi)
+
+#################################################################
+
+def MonteCarlo(n):
+  import random
+  k = 0 
+  for i in range(n):
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+    if x**2 + y**2 <= 1:
+      k += 1
+  return 4*k/n
+  
+print(MonteCarlo(5))
+print(MonteCarlo(50))
+print(MonteCarlo(500))
+print(MonteCarlo(5000))
